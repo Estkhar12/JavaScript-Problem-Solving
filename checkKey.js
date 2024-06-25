c6accd0005519c42dd146e3430a77daff930d985
Estkhar Ansari <estkharansari123@gmail.com>
2024-06-25 09:52:54 +0530

    Check if a Key Exists in an Object

---
+++ checkKey.js
@@ -0,0 +1,11 @@
+let obj = {
+    id: 1,
+    remarks: "Good"
+}
+
+// check for the keys 
+let result1 = obj.hasOwnProperty('name');
+let result2 = obj.hasOwnProperty('remarks')
+
+console.log(result1)
+console.log(result2)

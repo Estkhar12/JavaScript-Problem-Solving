c1b41422ce974d3ced9503a2884f4c808bca24ec
Estkhar Ansari <estkharansari123@gmail.com>
2024-06-25 09:51:35 +0530

    Check if An Array Contains a Specified Value

---
+++ arraySpecifiedValue.js
@@ -0,0 +1,9 @@
+const array = [ "you", "are", "going", "to", "Delhi"];
+
+const hasValue = array.includes("Delhi");
+
+if(hasValue){
+    console.log("Array contain a value.")
+}else{
+    console.log("Array does not contain a value.")
+}

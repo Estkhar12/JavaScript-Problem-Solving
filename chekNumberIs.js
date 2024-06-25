0dd7ef9bd58c662a67dca13fa6b3ec1085a407d7
Estkhar Ansari <estkharansari123@gmail.com>
2024-06-25 09:54:29 +0530

    Check if a number is Positive, Negative, or Zero

---
+++ chekNumberIs.js
@@ -0,0 +1,10 @@
+const number = parseInt(prompt("Enter Number: "));
+
+if (number > 0) {
+  console.log("Positive Number: ");
+} else if (number == 0) {
+  console.log("Zero Number");
+}
+if (number < 0) {
+  console.log("Negative Number");
+}

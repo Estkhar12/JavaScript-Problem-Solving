4ee741bbdf6640d15334441991a68e326d3c18f8
Estkhar Ansari <estkharansari123@gmail.com>
2024-06-25 09:49:46 +0530

    Append an Object to an Array

---
+++ appendObjArray.js
@@ -0,0 +1,10 @@
+function addObj(arr, obj) {
+  let index = arr.length;
+  arr.splice(index, 0, object);
+  console.log(arr);
+}
+let array = [1, 2, 3];
+
+let object = { x: 14, y: 42 };
+
+addObj(array, object);
